3ac2280dd09b7a6e53855cfda48ed1409fe8416c
Abhimishra2907 <mishraabhishek2907@gmail.com>
2026-06-09 19:21:08 +0530

    Product detail folder Added

---
+++ Java-core-projects/Product.java
@@ -0,0 +1,46 @@
+  class Product{
+  
+   private int id;
+   private String name;
+   private long price;
+   private String category;
+   
+   
+   // Constructor
+  public Product( int id, 
+                  String name, 
+                  long price,
+				  String category)
+				                    {
+	                this.id = id;
+		            this.name = name;
+		            this.price  = price;
+		            this.category = category;
+                                    }
+  
+  public String toString(){
+	   return
+	   " Product Detail : \n" + 
+	   "Id : " + id + "\n" + 
+	   "Name : " + name + "\n" +
+	   "Price : " + price + "\n" +
+	   "Category : " + category + "\n" ;
+	   
+  }
+  
+  
+  public static void main(String [] args){
+	  
+	  
+   Product p1 =
+            new Product(101,"Laptop",350000,"Electronic");
+
+        Product p2 =
+            new Product(102,"iPhone",65000,"SmartPhone");
+    
+	
+  System.out.println(p1.toString());
+  System.out.println(p2.toString());
+  
+  }
+  }
